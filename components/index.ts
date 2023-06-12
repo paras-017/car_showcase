5454511c2da6c30701929d088b170921afff2205
paras-017 <paraschauhan0017@gmail.com>
2023-06-12 12:20:13 +0530

    navbar and footer added

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 //  we're going to use this file as a single file that's going to export all of our components so we can import them in a simpler manner
 import Hero from './Hero'
+import Navbar from './Navbar'
+import Footer from './Footer'
 import CustomButton from './CustomButton'
 export {
     Hero,
-    CustomButton
+    CustomButton,
+    Navbar,
+    Footer
 }
--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,6 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps{
     title: string,
     containerStyles?:string,
-    handleClick:MouseEventHandler<HTMLButtonElement>
-
+    handleClick:MouseEventHandler<HTMLButtonElement>,
+    btnType?: 'button'|'submit'
 }
