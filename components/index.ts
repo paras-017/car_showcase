2bbde8eb4429a5513cc678bce9338ceba6e53e7a
paras-017 <paraschauhan0017@gmail.com>
2023-06-12 08:42:57 +0530

    hero section

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+//  we're going to use this file as a single file that's going to export all of our components so we can import them in a simpler manner
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+export {
+    Hero,
+    CustomButton
+}
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from "react";
+
+// Interface specifies how a specific structure should look like
+export interface CustomButtonProps{
+    title: string,
+    containerStyles?:string,
+    handleClick:MouseEventHandler<HTMLButtonElement>
+
+}
